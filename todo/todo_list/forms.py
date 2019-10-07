f7d37150185bf60b4fefcb129f12a22a144cae40
Aadesh Kumar <aadesh.stark@gmail.com>
2019-10-08 00:56:52 +0530

    modified index and improved UI

--- todo/todo_list/forms.py
+++ todo/todo_list/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class ToDoForm(forms.Form):
-	content = forms.CharField(max_length = 40)
+	content = forms.CharField(max_length = 40,
+		widget=forms.TextInput(attrs={'class':"form-control", 'placeholder':'Enter Your Task'} ))
